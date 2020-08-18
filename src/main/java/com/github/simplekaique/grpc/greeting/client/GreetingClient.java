021b5ea14cae0d9efc0617b9907a44baf7866553
Kaique Santos Leao <kleao@uolinc.com>
2020-08-17 21:56:52 -0300

    creating a client greeting

--- src/main/java/com/github/simplekaique/grpc/greeting/client/GreetingClient.java
+++ src/main/java/com/github/simplekaique/grpc/greeting/client/GreetingClient.java
@@ -1,6 +1,6 @@
 package com.github.simplekaique.grpc.greeting.client;
 
-import com.proto.dummy.DummyServiceGrpc;
+import com.proto.dummy.*;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 
@@ -13,9 +13,28 @@ public class GreetingClient {
                 .build();
 
         System.out.println("Creating stub");
-        DummyServiceGrpc.DummyServiceBlockingStub syncClient = DummyServiceGrpc.newBlockingStub(channel);
 
+        //DummyServiceGrpc.DummyServiceBlockingStub syncClient = DummyServiceGrpc.newBlockingStub(channel);
         //DummyServiceGrpc.DummyServiceFutureStub assyncClient = DummyServiceGrpc.newFutureStub(channel);
+
+        // created a greet service client (blocking - synchronous)
+        GreetServiceGrpc.GreetServiceBlockingStub greetClient = GreetServiceGrpc.newBlockingStub(channel);
+
+        // created a protocol buffer greeting message
+        Greeting greeting = Greeting.newBuilder()
+                .setFirstName("Kaique")
+                .setLastName("Leão")
+                .build();
+
+        // do the same for a GreetRequest
+        GreetRequest greetRequest = GreetRequest.newBuilder()
+                .setGreeting(greeting)
+                .build();
+
+        // call the RPC and get back a GreeResponse (protocol buffers)
+        GreetResponse greetResponse = greetClient.greet(greetRequest);
+
+        System.out.println(greetResponse.getResult());
 
         System.out.println("Shutting down channel");
         channel.shutdown();
